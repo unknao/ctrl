2a0b7ffa44ead55223f8b98776f10e9dc824e3b0
unknao <paujurc@gmail.com>
2024-01-10 19:51:47 +0200

    Update countdown.lua

--- lua/ctrl/modules/countdown.lua
+++ lua/ctrl/modules/countdown.lua
@@ -1,68 +1,68 @@
-if SERVER then 
-	
+if SERVER then
+
 	util.AddNetworkString("ctrl.countdown")
 	util.AddNetworkString("ctrl.abort")
-	
+
 	require("finishedloading")
-	
+
 	local CountdownStarted = RealTime()
-	
+
 	function ctrl.countdown(duration, name, fancy, func, ...)
-		
-		ctrl.msg("Countdown started, duration: "..duration.." seconds")
+
+		ctrl.msg("Countdown started, duration: " .. duration .. " seconds")
 		local duration = duration
 		local remainder = duration
 		CountdownStarted = RealTime()
 		local vararg = {...}
-		
+
 		net.Start("ctrl.countdown")
 		net.WriteString(name)
 		net.WriteFloat(duration)
 		net.WriteFloat(remainder)
 		net.WriteBool(fancy)
 		net.Broadcast()
-		
+
 		--Do not exclude the people connecting mid countdown
 		hook.Add("FinishedLoading", "ctrl.countdown", function(ply)
-			
+
 			local TimeRemaining = (CountdownStarted + duration) - RealTime()
 			if TimeRemaining <= 0 then return end
-			
+
 			net.Start("ctrl.countdown")
 			net.WriteString(name)
 			net.WriteFloat(TimeRemaining)
 			net.WriteFloat(remainder)
 			net.WriteBool(fancy)
 			net.Send(ply)
-			
+
 		end)
-		
+
 		timer.Create("ctrl.countdown", duration, 1, function()
-			
+
 			ctrl.msg("Countdown ended.")
 			func(unpack(vararg))
 			hook.Remove("FinishedLoading", "ctrl.countdown")
-			
+
 		end)
-		
+
 	end
-	
+
 	function ctrl.abort()
-		
+
 		ctrl.msg("Countdown aborted.")
 		timer.Stop("ctrl.countdown")
 		hook.Remove("FinishedLoading", "ctrl.countdown")
 		net.Start("ctrl.abort")
 		net.Broadcast()
-		
+
 	end
-	
+
 end
 
-if CLIENT then 
-	
+if CLIENT then
+
 	local fancylines = {
-		
+
 		[180] = "npc/overwatch/cityvoice/fcitadel_3minutestosingularity.wav",
 		[120] = "npc/overwatch/cityvoice/fcitadel_2minutestosingularity.wav",
 		[60] = "npc/overwatch/cityvoice/fcitadel_1minutetosingularity.wav",
@@ -73,9 +73,9 @@ if CLIENT then
 		[3] = "npc/overwatch/radiovoice/three.wav",
 		[2] = "npc/overwatch/radiovoice/two.wav",
 		[1] = "npc/overwatch/radiovoice/one.wav",
-		
+
 	}
-	
+
 	surface.CreateFont("ctrl.countdown", {
 		font = "Arial",
 		outline = true,
@@ -83,7 +83,7 @@ if CLIENT then
 		weight = 1200,
 		extended = true,
 	})
-	
+
 	surface.CreateFont("ctrl.countdownname", {
 		font = "Arial",
 		outline = true,
@@ -92,22 +92,22 @@ if CLIENT then
 		extended = true,
 	})
 	net.Receive("ctrl.countdown", function()
-		
+
 		local CountdownStarted = RealTime()
-		
+
 		local name = net.ReadString()
 		local duration = net.ReadFloat()
 		local remainder = net.ReadFloat()
 		local fancy = net.ReadBool()
-		
-		hook.Add("HUDPaint", "ctrl.countdown", function() 
-			
+
+		hook.Add("HUDPaint", "ctrl.countdown", function()
+
 			local TimeRemaining = ((CountdownStarted + duration) - RealTime())
 			if TimeRemaining < 0 then return end
-			
+
 			draw.RoundedBox(0, 0, 0, ScrW() * (TimeRemaining / remainder) + 2, 7, color_black)
 			draw.RoundedBox(0, 0, 0, ScrW() * (TimeRemaining / remainder), 5, color_white)
-			
+
 			draw.Text({
 				text = name,
 				font = "ctrl.countdownname",
@@ -115,7 +115,7 @@ if CLIENT then
 				yalign = 4,
 				pos = {ScrW() / 2, 40}
 			})
-			
+
 			draw.Text({
 				text = string.FormattedTime(TimeRemaining, "%02i:%02i.%02i"),
 				font = "ctrl.countdown",
@@ -123,11 +123,11 @@ if CLIENT then
 				yalign = 3,
 				pos = {ScrW() / 2, 40}
 			})
-			
+
 		end)
-		
+
 		if fancy then
-			
+
 			local fancysounds = {
 				"ambient/atmosphere/city_tone.wav",
 				"ambient/atmosphere/underground.wav",
@@ -138,47 +138,47 @@ if CLIENT then
 				"ambient/atmosphere/cargo_hold1.wav",
 			}
 			ctrl.Sounds = ctrl.Sounds or {}
-			
+
 			if table.IsEmpty(ctrl.Sounds) then
 				for i, v in ipairs(fancysounds) do
 					ctrl.Sounds[i] = CreateSound(game.GetWorld(), v)
 					if ctrl.Sounds[i] then ctrl.Sounds[i]:SetSoundLevel(0) end
 				end
 			end
-			
+
 			local SoundStartTime = math.max(remainder - 10, 0)
 			timer.Create("ctrl.countdown.fancysounds", SoundStartTime, 1, function()
 				for i, v in ipairs(ctrl.Sounds) do
 					v:PlayEx(0, 100)
 				end
 			end)
-			
+
 			local LastTime, Time = 0, 0
 			local BeamPos1, BeamPos2 = Vector(0, 0, 0), Vector(0, 0, 0) --Caching for faster runtime
-			
+
 			--Make it look like the end of the world.
 			hook.Add("PostDrawTranslucentRenderables", "ctrl.countdown", function(bDepth, bSkybox)
-				
-				local TimeRemaining = math.max(((CountdownStarted + duration) - RealTime()), 0)
+
+				local TimeRemaining = math.max((CountdownStarted + duration) - RealTime(), 0)
 				local Fraction = math.min(TimeRemaining / math.min(remainder, 10), 1)
-				
+
 				LastTime = Time
 				Time = math.ceil(TimeRemaining)
-				
+
 				if LastTime ~= Time and fancylines[Time] then
 					surface.PlaySound(fancylines[Time])
 				end
-				
+
 				if Fraction == 1 then return end
-				
+
 				util.ScreenShake(vector_origin, math.ease.InExpo(1 - Fraction) * 6, 50, 0.1, 0)
-				
+
 				for i, v in ipairs(ctrl.Sounds) do
 					v:ChangeVolume(math.ease.InExpo(1 - Fraction))
 				end
-				
+
 				render.SetColorMaterial()
-				local multi = math.ease.OutExpo(1 - math.max((1 - Fraction) * 1.1 - 1, 0) * 10)
+				local mult = math.ease.OutExpo(1 - math.max((1 - Fraction) * 1.1 - 1, 0) * 10)
 
 				surface.SetDrawColor(255 * mult, 255 * mult, 255 * mult, 255 * math.min((1 - Fraction) * 1.2, 1))
 				for _ = 1, math.floor(math.ease.InCirc(1 - Fraction) * 5000) do
@@ -187,15 +187,15 @@ if CLIENT then
 					BeamPos2:SetUnpacked(x, y, 32767)
 					render.DrawBeam(BeamPos1, BeamPos2, 5, 1, 1, DrawColor)
 				end
-				
+
 				render.CullMode(1)
 				render.DrawSphere(EyePos(), 10 + (32000 * Fraction) * (bSkybox and 0.0625 or 1), 50, 50, DrawColor)
 				render.CullMode(0)
 			end)
 		end
-		
+
 		timer.Create("ctrl.countdown", duration, 1, function()
-			
+
 			surface.PlaySound("buttons/button1.wav")
 			hook.Remove("HUDPaint", "ctrl.countdown")
 			timer.Simple(2, function()
@@ -203,20 +203,20 @@ if CLIENT then
 				for i, v in ipairs(ctrl.Sounds) do v:Stop() end
 				surface.PlaySound("buttons/lightswitch2.wav")
 			end)
-			
+
 		end)
-		
+
 	end)
-	
+
 	net.Receive("ctrl.abort", function()
-		
+
 		hook.Remove("HUDPaint", "ctrl.countdown")
 		hook.Remove("PostDrawTranslucentRenderables", "ctrl.countdown")
 		timer.Stop("ctrl.countdown")
 		timer.Stop("ctrl.countdown.fancysounds")
-		
+
 		for i, v in ipairs(ctrl.Sounds) do v:Stop() end
 		surface.PlaySound("buttons/button1.wav")
-		
+
 	end)
 end

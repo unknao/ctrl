2322ab63e35b25ffbda32766ec2727bd5882f1ee
unknao <paujurc@gmail.com>
2024-08-11 09:41:16 +0300

    removed debug prints

--- lua/ctrl/modules/damagemodes/godmortal.lua
+++ lua/ctrl/modules/damagemodes/godmortal.lua
@@ -6,11 +6,9 @@ end)
 
 ctrl.registerDamageMode("only mortals can hurt you", false, false, function(ply, attacker, dmg) --Only mortals can hurt you
 	if not attacker:IsPlayer() then
-		print(1)
 		attacker = attacker:CPPIGetOwner()
 	end
 
-	print(2)
 	if dmg:GetDamageCustom() == 1337 then return end
 
 	return attacker:HasGodMode()
